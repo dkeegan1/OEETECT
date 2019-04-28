c3a621f14620578c9e0445678a600f78b99e1e66
Darren Keegan <dkeegan121@gmail.com>
2019-04-28 17:37:15 +0100

    Website styling completed

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # OEETECT
-Update 26/04/2019
+Update 28/04/2019
 
 Aggregation results are now stored in a collection called results.
 Collection is then called and is displayed on webpage.
+Website is now styled correctly.
+
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,10 @@ var mongodb = require('mongodb');
 var addSubtractDate = require("add-subtract-date");
 var csrf = require('csurf');
 var csrfProtection = csrf();
-mongoose.connect('localhost:27017/test');
 var Schema = mongoose.Schema;
 var assert = require('assert');
 var passport = require('passport');
-
-
+mongoose.connect('localhost:27017/test');
 var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:27017/test';
 MongoClient.connect(url, (err, db) => {
@@ -20,18 +18,50 @@ MongoClient.connect(url, (err, db) => {
     });
 });
 var sumDuration = (db, callback) => {
- //var OEE = [ { $project:{_id:0, OEE:{$trunc:[ {$multiply:[{$divide:[{$sum:[ "$duration"] },10080]},100]}]}}}];
- //,{ $out : "Results" }
   var agr = [{$group: { total: {$sum: "$duration"},_id:"Total Duration"}},{ $out : "Results" }];// adds all the duration and creates collection Results
-  var Mechanical = [{ $match: {$or: [ { event: "Mechanical" }] }},{ $group: {_id: "Mechanical", total: {$sum: "$duration" } }}]; //adds all Mechanical
-  var CIP = [{ $match: {$or: [{ event: "CIP" }] }},{ $group: {_id: "CIP", total: { $sum: "$duration" } }}];//adds all cips
-  var List = [{ $match: {$or: [{ event: "List Change" }] }},{ $group: {_id: "List Change", total: { $sum: "$duration" } }}]; //adds all list changes
- //result = 3;
-    var cursor = db.collection('dryer1').aggregate(agr).toArray( (err, res) => { //jason object
-       assert.equal(err, null);
-       console.log("DURATION CALCULATIONS");
+  var Mechanical = [{ $match: {$or: [ { event: "Mechanical" }] }},{ $group: {_id: "Mechanical", total: {$sum: "$duration" } }},{ $out : "Mechanical" }]; //adds all Mechanical
+  var CIP = [{ $match: {$or: [{ event: "CIP" }] }},{ $group: {_id: "CIP", total: { $sum: "$duration" } }},{ $out : "CIP" }];//adds all cips
+  var List = [{ $match: {$or: [{ event: "List Change" }] }},{ $group: {_id: "List_Change", total: { $sum: "$duration" } }},{ $out : "ListChange" }]; //adds all list changes
+  var Scheduale = [{ $match: {$or: [{ event: "Schedualed Downtime" }] }},{ $group: {_id: "Schedualed Downtime", total: { $sum: "$duration" } }},{ $out : "SchedualedDowntime" }];
+
+ var cursor = db.collection('dryer1').aggregate(agr).toArray( (err, res) => {
+    assert.equal(err, null);
+ });
+ var cursor = db.collection('dryer1').aggregate(Mechanical).toArray( (err, res) => {
+    assert.equal(err, null);
+ });
+ var cursor = db.collection('dryer1').aggregate(CIP).toArray( (err, res) => {
+    assert.equal(err, null);
+ });
+ var cursor = db.collection('dryer1').aggregate(List).toArray( (err, res) => {
+    assert.equal(err, null);
+ });
+ var cursor = db.collection('dryer1').aggregate(Scheduale).toArray( (err, res) => {
+   console.log(res);
+    assert.equal(err, null);
+ });
+};
+
+/*MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("test");
+  dbo.collection('Results').aggregate([
+    { $lookup:
+       {
+         from: 'Mechanical',
+         localField: '_id',
+         foreignField: 'total',
+         as: 'Added'
+       }
+     }
+
+    ]).toArray(function(err, res) {
+    if (err) throw err;
+    console.log(JSON.stringify(res));
+    db.close();
   });
-}
+});*/
+
 
 router.get('/graph', function(req, res, next) { //welcome Page have to do this one yet
     var resultArray = [];
@@ -43,10 +73,67 @@ router.get('/graph', function(req, res, next) { //welcome Page have to do this o
         resultArray.push(doc);
       }, function(){
         db.close();
-        res.render('D1graph',{items: resultArray});
+        res.render('graphs/D1graph',{items: resultArray});
       });
     });
 });
+router.get('/mechanical', function(req, res, next) { //welcome Page have to do this one yet
+    var resultArray = [];
+    mongodb.connect(url,function(err, db){
+      assert.equal(null, err);
+      var cursor = db.collection('Mechanical').find();
+      cursor.forEach(function(doc, err){
+        assert.equal(null, err);
+        resultArray.push(doc);
+      }, function(){
+        db.close();
+        res.render('graphs/Mech',{items: resultArray});
+      });
+    });
+});
+router.get('/cip', function(req, res, next) { //welcome Page have to do this one yet
+    var resultArray = [];
+    mongodb.connect(url,function(err, db){
+      assert.equal(null, err);
+      var cursor = db.collection('CIP').find();
+      cursor.forEach(function(doc, err){
+        assert.equal(null, err);
+        resultArray.push(doc);
+      }, function(){
+        db.close();
+        res.render('graphs/Cip',{items: resultArray});
+      });
+    });
+});
+router.get('/ListChange', function(req, res, next) { //welcome Page have to do this one yet
+    var resultArray = [];
+    mongodb.connect(url,function(err, db){
+      assert.equal(null, err);
+      var cursor = db.collection('ListChange').find();
+      cursor.forEach(function(doc, err){
+        assert.equal(null, err);
+        resultArray.push(doc);
+      }, function(){
+        db.close();
+        res.render('graphs/ListChange',{items: resultArray});
+      });
+    });
+});
+router.get('/Scheduale', function(req, res, next) { //welcome Page have to do this one yet
+    var resultArray = [];
+    mongodb.connect(url,function(err, db){
+      assert.equal(null, err);
+      var cursor = db.collection('SchedualedDowntime').find();
+      cursor.forEach(function(doc, err){
+        assert.equal(null, err);
+        resultArray.push(doc);
+      }, function(){
+        db.close();
+        res.render('graphs/Scheduale',{items: resultArray});
+      });
+    });
+});
+
 
 var userDataSchema = new Schema({
   event: {type: String, required: true},
@@ -64,7 +151,7 @@ var Evap1 = mongoose.model('Evapartor1', userDataSchema);
 var Evap2 = mongoose.model('Evapartor2', userDataSchema);
 
 router.use(csrfProtection);
-/* GET home page. */
+
 router.get('/user/signup', function(req, res, next) { //Login Page
   var messages = req.flash('error');
   res.render('user/signup', {csrfToken: req.csrfToken(), messages:messages});
@@ -75,22 +162,31 @@ router.post('/user/signup',passport.authenticate('local.signup',{
   failureFlash: true
 }));
 
+router.get('/user/signin', function(req, res, next) { //Login Page
+  var messages = req.flash('error');
+  res.render('user/signin', {csrfToken: req.csrfToken(), messages:messages});
+});
+router.post('/user/signin',passport.authenticate('local.signin',{
+  successRedirect: '/Welcome',
+  failureRedirect: '/user/signin',
+  failureFlash: true
+}));
+
+router.get('/user/logout', function(req, res, next) { //Login Page
+  req.logout();
+  res.redirect('user/signin');
+});
 
 router.get('/', function(req, res, next) { //Login Page
-  res.render('index');
+  res.render('Wel');
 });
-router.get('/Welcome', function(req, res, next) { //welcome Page have to do this one yet
-      res.render('Wel');
-});
+
 router.get('/enterD1', function(req, res, next) {
    //Insert Page
   res.render('insert/Dryer1');
 });
-/*router.get('/graph', function(req, res, next) {
-   //Insert Page
-  res.render('D1graph');
-});*/
-router.get('/enterD2', function(req, res, next) { //Insert Page
+
+router.get('/enterD2', isLoggedIn, function(req, res, next) { //Insert Page
   res.render('insert/Dryer2');
 });
 router.get('/enterE1', function(req, res, next) { //Insert Page
@@ -125,7 +221,6 @@ router.get('/getE2', function(req, res, next) { //Get Data Page
         res.render('display/E2', {items: doc});
       });
     });
-
 router.post('/insertD1', function(req, res, next) {
   var item = {
     event : req.body.event,
@@ -182,7 +277,6 @@ router.post('/insertE2', function(req, res, next) {
   data.save();
   res.redirect('/Welcome');
 });
-
 router.post('/delete', function(req, res, next) {
   var id = req.body.id;
   Dryer1.findByIdAndRemove(id).exec();
@@ -191,5 +285,17 @@ router.post('/delete', function(req, res, next) {
   Evap2.findByIdAndRemove(id).exec();
   res.redirect('/Welcome');
 });
-
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/'); //starting page
+}
+function notLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
